49366d48b9a000a12fa1e09bb9b35f7a7a396c5a
Juan C Peralta <juanc7peralta@gmail.com>
2025-11-14 11:05:50 -0300

    footerIncomming..

---
+++ src/assets/icons/CineRateLogo.jsx
@@ -0,0 +1,23 @@
+const CineRateLogo = () => {
+  return (
+    <svg className="opacity-70" viewBox="0 0 400 100" xmlns="http://www.w3.org/2000/svg">
+      
+      {/* Texto CINE */}
+      <text x="90" y="62" 
+            fontFamily="Arial, sans-serif" 
+            fontSize="40" 
+            fontWeight="bold" 
+            fill="#ffffff"
+            letterSpacing="3">Cine</text>
+      
+      {/* Texto RATE */}
+      <text x="200" y="62" 
+            fontFamily="Arial, sans-serif" 
+            fontSize="40" 
+            fontWeight="bold" 
+            fill="#0ed395"
+            letterSpacing="3">Rate</text>
+    </svg>
+  );
+};
+export default CineRateLogo;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
-
 export const Footer = () => {
   return (
-    <footer className=" py-4 text-center text-gray-400">
-      <div>
-        <p>Hola</p>
+    <footer className="text-center text-gray-400 bg-[var(--bgSecondary)]  w-full max-w-[100vw] overflow-hidden h-[200px] flex items-center justify-between relative">
+      <div className='grid grid-cols-3 w-full z-10'>
+        <div className='flex flex-col justify-start  items-center'>
+          <span className='text-xl mb-2 uppercase '>Información</span>
+          <span className=''>Sobre nosotros</span>
+          <span>Politica de privacidad</span>
+          <span>Ayuda</span>
+        </div>
+        <div className='flex flex-col justify-start  items-center'>
+                    <span className='text-xl mb-2 uppercase'>Navegación</span>
+          <span>Home</span>
+          <span>Populars Series</span>
+          <span>Premiere Series</span>
+          <span>Top Ten Series</span>
+        </div>
+        <div className='flex flex-col justify-start  items-center'>
+                    <span className='text-xl mb-2 uppercase'>Cine Rate</span>
+          <span>@CopyRight CineRate</span>
+          <span>@Github</span>
+          <span>@API propia</span>
+        </div>
       </div>
     </footer>
   )
